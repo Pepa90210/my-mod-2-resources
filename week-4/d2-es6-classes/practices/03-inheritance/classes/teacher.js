20f16ad03d03d48e742a18320c682879fb84bc21
Daniel Hoffmann <danielhoffmann1@gmail.com>
2022-06-01 13:21:48 -0500

    completed w4 d2 03-inheritance

--- week-4/d2-es6-classes/practices/03-inheritance/classes/teacher.js
+++ week-4/d2-es6-classes/practices/03-inheritance/classes/teacher.js
@@ -1,6 +1,19 @@
 const Person = require('./person');
 
-// Your code here
+class Teacher extends Person {
+  constructor(firstName, lastName, subject, yearsOfExperience) {
+    super(firstName, lastName);
+    this.subject = subject;
+    this.yearsOfExperience = yearsOfExperience;
+  };
+
+  static combinedYearsOfExperience(teachers) {
+    return teachers.reduce((accum, el) => {
+      return accum + el.yearsOfExperience
+    }, 0)
+  };
+
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
