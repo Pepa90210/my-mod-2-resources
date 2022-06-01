8726facd668d5c55a32e97636980f6a284d24638
Daniel Hoffmann <danielhoffmann1@gmail.com>
2022-06-01 12:10:04 -0500

    completed w4 d2 02

--- week-4/d2-es6-classes/practices/02-basic-class-syntax/classes/person.js
+++ week-4/d2-es6-classes/practices/02-basic-class-syntax/classes/person.js
@@ -1,5 +1,35 @@
 // Your code here
 
+class Person {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  };
+
+  introduce() {
+    console.log(`Hi, I'm ${this.firstName} ${this.lastName}, and I'm ${this.age} years old.`)
+  };
+
+  static introducePeople(array) {
+    if (!Array.isArray(array)) {
+      console.log(`introducePeople only takes an array as an argument.`);
+    } else if (array.some(el => !(el instanceof Person))) {
+      console.log(`All items in array must be Person class instances.`);
+    } else {
+      array.forEach(el => el.introduce());
+    }
+  };
+}
+
+// const daniel = new Person(
+//   'Daniel',
+//   'Hoffmann',
+//   31
+// );
+
+// daniel.introduce();
+
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
