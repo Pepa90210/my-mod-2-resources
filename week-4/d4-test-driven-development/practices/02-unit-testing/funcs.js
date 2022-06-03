b45e8d4ce67f7354b4f38cd0d512acbbc363da4e
Daniel Hoffmann <danielhoffmann1@gmail.com>
2022-06-03 11:11:37 -0500

    completed w4 d4 02-unit-testing funcs

--- week-4/d4-test-driven-development/practices/02-unit-testing/funcs.js
+++ week-4/d4-test-driven-development/practices/02-unit-testing/funcs.js
@@ -1,13 +1,24 @@
 function isFive(num) {
-  // Your code here
+  if (num === 5) return true;
+  else return false;
 }
 
 function isOdd(number) {
-  // Your code here
+  if (typeof number !== 'number') throw new Error('Invalid')
+  else if (number % 2 === 0) return false;
+  else return true;
 }
 
 function myRange(min, max, step = 1) {
-  // Your code here
+  let results = [];
+
+  if (min <= max) {
+    for (let i = min; i <= max; i += step) {
+      results.push(i);
+    }
+  }
+
+  return results;
 }
 
 
