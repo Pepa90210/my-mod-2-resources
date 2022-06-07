29881bb201295e82d98ce5d7b302b54b79f00889
Daniel Hoffmann <danielhoffmann1@gmail.com>
2022-06-07 13:30:46 -0500

    completed w5 d2 prac 03

--- week-5/d2-binary-hexa-ascii-memory-arrays/practices/03-dynamic-arrays/dynamic-array.js
+++ week-5/d2-binary-hexa-ascii-memory-arrays/practices/03-dynamic-arrays/dynamic-array.js
@@ -1,21 +1,27 @@
 class DynamicArray {
 
   constructor(defaultSize=4) {
-
-    // Your code here
-
-  }
+    this.capacity = defaultSize;
+    this.length = 0;
+    this.data = new Array(this.capacity);
+  };
 
   read(index) {
-
-    // Your code here
-  }
+    return this.data[index];
+  };
 
   unshift(val) {
+    this.length++;
+
+    for (let i = this.length; i > 0; i--) {
+      this.data[i] = this.data[i - 1];
+    }
+
+    this.data[0] = val;
 
     // Your code here
-  }
-
+    return this.length;
+  };
 }
 
 
