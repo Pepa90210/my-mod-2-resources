ce31d473735e6241117d2d01ada15b0e7cdfd033
Daniel Hoffmann <danielhoffmann1@gmail.com>
2022-06-14 13:12:24 -0500

    completed w6 d2 short practices

--- week-6/d2-binary-search-merge-quicksort/practices/02-merge-sort/merge-sort.js
+++ week-6/d2-binary-search-merge-quicksort/practices/02-merge-sort/merge-sort.js
@@ -3,15 +3,19 @@
 function mergeSort(arr) {
 
   // Check if the input is length 1 or less
-    // If so, it's already sorted: return
+  // If so, it's already sorted: return
+  if (arr.length <= 1) return arr;
 
   // Divide the array in half
+  let leftSide = arr.slice(0, (arr.length / 2));
+  let rightSide = arr.slice(arr.length / 2);
 
   // Recursively sort the left half
   // Recursively sort the right half
-
+  leftSide = mergeSort(leftSide);
+  rightSide = mergeSort(rightSide);
   // Merge the halves together and return
-
+  return merge(leftSide, rightSide)
 }
 
 
@@ -19,15 +23,33 @@ function mergeSort(arr) {
 function merge(arrA, arrB) {
 
   // Create an empty return array
+  let merged = [];
 
   // Point to the first value of each array
+  let indexA = 0;
+  let indexB = 0;
   // While there are still values in each array...
+  while (indexA < arrA.length && indexB < arrB.length) {
     // Compare the first values of each array
     // Add the smaller value to the return array
     // Move the pointer to the next value in that array
+    if (arrA[indexA] <= arrB[indexB]) {
+      merged.push(arrA[indexA]);
+      indexA++;
+    } else {
+      merged.push(arrB[indexB]);
+      indexB++;
+    }
+  };
+
+  if (indexA === arrA.length && indexB < arrB.length) {
+    merged.push(...arrB.slice(indexB));
+  } else if (indexA < arrA.length && indexB === arrB.length) {
+    merged.push(...arrA.slice(indexA));
+  };
 
   // Return the return array
-
+  return merged;
 }
 
 module.exports = [merge, mergeSort];
--- week-6/d2-binary-search-merge-quicksort/practices/03-quicksort/quicksort.js
+++ week-6/d2-binary-search-merge-quicksort/practices/03-quicksort/quicksort.js
@@ -1,20 +1,36 @@
-function quicksort(arr) {
+// function quicksort(arr) {
 
-  // Check if the input is length 1 or less
-    // If so, it's already sorted: return
+//   // Check if the input is length 1 or less
+//   // If so, it's already sorted: return
+//   if (arr.length <= 1) return arr;
 
-  // Pick the first value as the pivot
+//   // Pick the first value as the pivot
+//   let pivot = arr[0];
 
-  // Orient the pivot so that...
-      // every number smaller than the pivot is to the left
-      // every number larger (or equal) than the pivot is to the right
+//   // Orient the pivot so that...
+//   // every number smaller than the pivot is to the left
+//   // every number larger (or equal) than the pivot is to the right
 
-  // Recursively sort the left
-  // Recursively sort the right
+//   let left = [];
+//   let right = [];
 
-  // Return the left, pivot and right in sorted order
+//   for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] <= pivot) {
+//       left.push(arr[i]);
+//     } else {
+//       right.push(arr[i]);
+//     }
+//   }
 
-}
+//   // Recursively sort the left
+//   // Recursively sort the right
+
+//   // Return the left, pivot and right in sorted order
+//   return [...quicksort(left), pivot, ...quicksort(right)];
+
+// }
+
+const quicksort = arr => arr.length <= 1 ? arr : [...quicksort(arr.slice(1).filter(el => el <= arr[0])), arr[0], ...quicksort(arr.slice(1).filter(el => el > arr[0]))];
 
 
 module.exports = [quicksort];
